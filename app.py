e993a9901e957076f6774cb5dd535e7473037c60
kainklob <klobk23@gmail.com>
2022-06-22 16:13:07 -0400

    updated app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ scheduler = APScheduler()
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('HEROKU_POSTGRESQL_BLACK_URL', 'postgresql:///mebe'))
+app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgresql:///mebe'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
